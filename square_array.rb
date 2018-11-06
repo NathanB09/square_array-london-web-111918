4f585b1860bfd1c4ebe134b098a2d012c68daa29
Nathan Bell <nathanbell09@gmail.com>
2018-11-06 18:08:51 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,9 @@
 def square_array(array)
   # your code here
-  squared_array = []
-  array.each do |num|
-    squared_array << num ** 2
-  end
-  squared_array
+  # squared_array = []
+  # array.each do |num|
+  #   squared_array << num ** 2
+  # end
+  # squared_array
+  array.square
 end
